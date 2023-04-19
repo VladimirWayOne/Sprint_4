59cd0855f00a43835ebcd0d147fc8f0248aa7c00
Rekin Vladimir <rekin.vladimir@yandex.ru>
2023-04-19 16:13:41 +0300

    Added new locators for OrderPage

--- utils/locators.py
+++ utils/locators.py
@@ -26,15 +26,20 @@ class YaScooterHomePageLocator:
 
 class YaScooterOrderPageLocator:
     FIRST_NAME_INPUT = [By.XPATH, ".//input[contains(@placeholder,'Имя')]"]
+    INCORRECT_FIRST_NAME_MESSAGE = [By.XPATH, ".//input[contains(@placeholder,'Имя')]/parent::div/div"]
     LAST_NAME_INPUT = [By.XPATH, ".//input[contains(@placeholder,'Фамилия')]"]
+    INCORRECT_LAST_NAME_MESSAGE = [By.XPATH, ".//input[contains(@placeholder,'Фамилия')]/parent::div/div"]
     ADDRESS_INPUT = [By.XPATH, ".//input[contains(@placeholder,'Адрес')]"]
+    INCORRECT_ADDRESS_MESSAGE = [By.XPATH, ".//input[contains(@placeholder,'Адрес')]/parent::div/div"]
     SUBWAY_FIELD = [By.XPATH, ".//input[contains(@placeholder,'метро')]"]  # /parent::div
+    INCORRECT_SUBWAY_MESSAGE = [By.XPATH, ".//input[contains(@placeholder,'метро')]/parent::div/parent::div/parent::div/div[@class!='select-search']"]
 
-    @staticmethod
     def SUBWAY_HINT_BUTTON(subway_name):
         return [By.XPATH, f".//div[text()='{subway_name}']/parent::button"]
 
     TELEPHONE_NUMBER_FIELD = [By.XPATH, ".//input[contains(@placeholder,'Телефон')]"]
+    INCORRECT_TELEPHONE_NUMBER_MESSAGE = [By.XPATH, ".//input[contains(@placeholder,'Телефон')]/parent::div/div"]
+
     NEXT_BUTTON = [By.XPATH, ".//button[text()='Далее']"]
     BACK_BUTTON = [By.XPATH, ".//button[text()='Назад']"]
     DATE_FIELD = [By.XPATH, ".//input[contains(@placeholder,'Когда')]"]
@@ -44,6 +49,7 @@ class YaScooterOrderPageLocator:
     COMMENT_FOR_COURIER_FIELD = [By.XPATH, ".//input[contains(@placeholder,'Комментарий для курьера')]"]
     ORDER_BUTTON = [By.XPATH, ".//button[text()='Назад']/parent::div/button[text()='Заказать']"]
     ACCEPT_ORDER_BUTTON = [By.XPATH, ".//button[text()='Да']"]
+    ORDER_COMPLETED_INFO = [By.XPATH, ".//div[contains(text(),'Номер заказа')]"]
 
 
 class YandexPageLocator:
