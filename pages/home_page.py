05c709977d1ba69b39026010369d47b1e1e37948
Rekin Vladimir <rekin.vladimir@yandex.ru>
2023-04-19 18:10:38 +0300

    Fixed step description

--- pages/home_page.py
+++ pages/home_page.py
@@ -2,6 +2,7 @@ from pages.base_page import BasePage
 from utils.locators import YaScooterHomePageLocator as Locators
 import allure
 
+
 class YaScooterHomePage(BasePage):
     @allure.step('Принять куки')
     def click_cookie_accept(self):
@@ -19,7 +20,7 @@ class YaScooterHomePage(BasePage):
     def click_yandex_button(self):
         return self.find_element(Locators.YANDEX_SITE_BUTTON).click()
 
-    @allure.step('Принять куки')
+    @allure.step('Нажать на вопрос в FAQ')
     def click_faq_question(self, question_number: int):
         elems = self.find_elements(Locators.FAQ_BUTTONS, 10)
         return elems[question_number].click()
