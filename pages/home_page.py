8d1ed193d864a88200b70d03ee34f304b2db174e
Rekin Vladimir <rekin.vladimir@yandex.ru>
2023-04-20 08:44:36 +0300

    Deleted extra func

--- pages/home_page.py
+++ pages/home_page.py
@@ -4,10 +4,6 @@ import allure
 
 
 class YaScooterHomePage(BasePage):
-    @allure.step('Принять куки')
-    def click_cookie_accept(self):
-        return self.find_element(Locators.COOKIE_ACCEPT_BUTTON).click()
-
     @allure.step('Нажать на кнопку заказа вверху страницы')
     def click_top_order_button(self):
         return self.find_element(Locators.TOP_ORDER_BUTTON).click()
@@ -15,10 +11,6 @@ class YaScooterHomePage(BasePage):
     @allure.step('Нажать на кнопку заказа внизу страницы')
     def click_bottom_order_button(self):
         return self.find_element(Locators.BOTTOM_ORDER_BUTTON).click()
-
-    @allure.step('Перейти на страницу яндекса')
-    def click_yandex_button(self):
-        return self.find_element(Locators.YANDEX_SITE_BUTTON).click()
 
     @allure.step('Нажать на вопрос в FAQ')
     def click_faq_question(self, question_number: int):
