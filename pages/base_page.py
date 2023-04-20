af9f27815b501dc69a2e5e5d9ecd19a6afe4474d
Rekin Vladimir <rekin.vladimir@yandex.ru>
2023-04-20 08:44:15 +0300

    Added new base_page func

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,4 @@
+import allure
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from utils.urls import Urls
@@ -17,19 +18,27 @@ class BasePage:
         return WebDriverWait(self.driver, time).until(EC.presence_of_all_elements_located(locator),
                                                       message=f"Can't find elements by locator {locator}")
 
+    @allure.step('Перейти по адресу')
     def go_to_site(self, url=None):
         if url is None:
             url = self.base_url
         return self.driver.get(url)
 
+    @allure.step('Получить текущий URL')
     def current_url(self):
         return self.driver.current_url
 
+    @allure.step('Переключиться на вкладку браузера')
     def switch_window(self, window_number: int = 1):
         return self.driver.switch_to.window(self.driver.window_handles[window_number])
 
     def wait_url_until_not_about_blank(self, time=10):
         return WebDriverWait(self.driver, time).until_not(EC.url_to_be('about:blank'))
 
+    @allure.step('Принять куки')
     def click_accept_cookie(self):
         return self.find_element(BasePageLocator.COOKIE_ACCEPT_BUTTON).click()
+
+    @allure.step('Перейти на страницу яндекса')
+    def click_yandex_button(self):
+        return self.find_element(BasePageLocator.YANDEX_SITE_BUTTON).click()
